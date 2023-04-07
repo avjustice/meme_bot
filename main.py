67864f529d024478cb4094670c85c808bd933364
avjustice <iatsenko.av@gmail.com>
2023-04-08 00:16:36 +0300

    Resized keyboard

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ async def start(update, context):
     logger.info('%s стартанул бота', user.first_name)
     await update.message.reply_text(
         f"Здравствуйте, {user.first_name}. Это бот со случайными мемами.\n",
-        reply_markup=ReplyKeyboardMarkup([['Покажи мемчик!']])
+        reply_markup=ReplyKeyboardMarkup([['Покажи мемчик!']], resize_keyboard=True)
     )
 
 
 async def meme(update, context):
     response = requests.get('https://meme-api.com/gimme').json()
-    await update.message.reply_photo(response['url'], reply_markup=ReplyKeyboardMarkup([['Еще мемчик!']]))
+    await update.message.reply_photo(response['url'],
+                                     reply_markup=ReplyKeyboardMarkup([['Еще мемчик!']], resize_keyboard=True))
     logger.info('%s смотрит мем %s', update.effective_user.first_name, response['url'])
 
 
